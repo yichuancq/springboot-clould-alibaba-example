6852933cddf46e0a6c02f8d647b4a2be3f26f449
yichuancq <1012027293@qq.com>
2020-03-29 17:24:37 +0800

    fix maven

--- nacos/nacos-config/src/main/java/config/NacosConfigApplication.java
+++ nacos/nacos-config/src/main/java/config/NacosConfigApplication.java
@@ -15,6 +15,7 @@ public class NacosConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosConfigApplication.class, args);
     }
+
     @NacosValue(value = "${nacos.test.propertie:123}", autoRefreshed = true)
     private String testProperties;
 
